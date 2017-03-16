acb206dd497f33acb707d4c88489cd8b4d768c0a
Orrin <zhangaolin@czcg.com.cn>
2017-03-16 15:15:35 +0800

    日志加彩蛋

--- spring-boot-study/spring-boot-dubbo/spring-boot-dubbo-demo-parent/spring-boot-dubbo-demo-consumer/src/main/java/com/xxx/sample/spring/boot/dubbo/demo/consumer/ConsumerApplication.java
+++ spring-boot-study/spring-boot-dubbo/spring-boot-dubbo-demo-parent/spring-boot-dubbo-demo-consumer/src/main/java/com/xxx/sample/spring/boot/dubbo/demo/consumer/ConsumerApplication.java
@@ -1,9 +1,14 @@
 package com.xxx.sample.spring.boot.dubbo.demo.consumer;
 
+import com.alibaba.dubbo.rpc.Invocation;
+import com.alibaba.dubbo.rpc.Invoker;
+import com.alibaba.dubbo.rpc.Result;
 import com.xxx.sample.spring.boot.dubbo.autoconfig.annotation.EnableDubbo;
+import com.xxx.sample.spring.boot.dubbo.core.filter.AbstractDubboConsumerFilterSupport;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.util.concurrent.CountDownLatch;
@@ -16,6 +21,11 @@ import java.util.concurrent.CountDownLatch;
 @EnableDubbo(basePackages = "com.xxx.sample.spring.boot.dubbo.demo.consumer")
 public class ConsumerApplication {
 
+	@Bean
+	ConsumerFilter consumerFilter() {
+		return new ConsumerFilter();
+	}
+
 	public static void main(String[] args) throws InterruptedException {
 		ApplicationContext ctx = new SpringApplicationBuilder()
 				.sources(ConsumerApplication.class)
@@ -23,6 +33,13 @@ public class ConsumerApplication {
 				.run(args);
 		ctx.getBean(ConsumerAction.class).add(21, 25);
 		new CountDownLatch(1).await();
+	}
+
+	static class ConsumerFilter extends AbstractDubboConsumerFilterSupport {
+		public Result invoke(Invoker<?> invoker, Invocation invocation) {
+			System.out.println("ConsumerFilter");
+			return invoker.invoke(invocation);
+		}
 	}
 
 }
